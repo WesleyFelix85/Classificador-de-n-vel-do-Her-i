0526dc577efff5e3970f98e6055f2c71731ccdb4
Antonio Wesley <wesleyfelix843@gmail.com>
2024-05-03 14:22:00 -0300

    commit 2

--- Classificador-de-nivel-do-heroi.js
+++ Classificador-de-nivel-do-heroi.js
@@ -1,32 +1,32 @@
   let nome = "Jogador 1"
-  let quantidadeExperiencia = 1000
+  let quantidadeExperiencia = 5000
   let nivel
 
-  if (quantidadeExpeiencia =< 1000) 
+  if (quantidadeExperiencia <= 1000) 
   {
     nivel = "Ferro"
   }
-  if (quantidadeExperiencia > 1000) && (quantidadeExperiencia <= 2000)
+  if (quantidadeExperiencia > 1000 && quantidadeExperiencia <= 2000)
   {
     nivel = "Bronze"
   }
-  if (quantidadeExperiencia > 2000) && (quantidadeExperiencia <= 5000)
+  if (quantidadeExperiencia > 2000 && quantidadeExperiencia <= 5000)
   {
     nivel = "Prata"
   }
-  if (quantidadeExperiencia > 5000) && (quantidadeExperiencia <= 7000)
+  if (quantidadeExperiencia > 5000 && quantidadeExperiencia <= 7000)
   {
     nivel = "Ouro"
   }
-  if (quantidadeExperiencia > 7000) && (quantidadeExperiencia <= 8000)
+  if (quantidadeExperiencia > 7000 && quantidadeExperiencia <= 8000)
   {
     nivel = "Platina"
   }
-  if (quantidadeExperiencia > 8000) && (quantidadeExperiencia <= 9000)
+  if (quantidadeExperiencia > 8000 && quantidadeExperiencia <= 9000)
   {
     nivel = "Ascendente"
   }
-  if (quantidadeExperiencia > 9000) && (quantidadeExperiencia <= 10000)
+  if (quantidadeExperiencia > 9000 && quantidadeExperiencia <= 10000)
   {
     nivel = "Imortal"
   }
